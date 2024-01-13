b6656e51db0cf731929d7f844ac9651db0bbdd53
khushi2204 <114248621+khushi2204@users.noreply.github.com>
2024-01-14 00:00:52 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+home=document.querySelector(".home"),
+login-box=document.querySelector(".login-box"),
+signup=document.querySelector(".signup"),
+login=document.querySelector("login"),
+
+formOpenBtn.addEventListener("click",()=> home.classList.add("show"));
+formCloseBtn.addEventListener("click",()=> home.classList.remove("show"));
